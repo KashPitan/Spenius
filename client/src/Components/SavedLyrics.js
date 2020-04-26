ad4e529dca9f71533519e1194b5c9f0a0cc507fe
Kash Pitan <kashpitan@googlemail.com>
2020-04-26 01:52:18 +0100

    styling changes: centred web page, aligned components, colou scheme, saved lyrics positioning, save lyrics button positioning etc.

--- client/src/Components/SavedLyrics.js
+++ client/src/Components/SavedLyrics.js
@@ -4,11 +4,14 @@ import SavedLyricObject from "./SavedLyricObject";
 const SavedLyrics = (props) => {
   // const { lyricObjects } = this.props;
   return (
-    <div className="savedLyrics">
-      {props.lyricObjects.map((lyricObj) => (
-        <SavedLyricObject key={lyricObj.id} savedLyricObject={lyricObj} />
-      ))}
-    </div>
+    <>
+      <hr></hr>
+      <div className="savedLyrics">
+        {props.lyricObjects.map((lyricObj) => (
+          <SavedLyricObject key={lyricObj.id} savedLyricObject={lyricObj} />
+        ))}
+      </div>
+    </>
   );
 };
 
