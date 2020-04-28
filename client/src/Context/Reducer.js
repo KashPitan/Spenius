2c3cbca4ab688dc10afc6e8238dac3fa1a46120f
Kash Pitan <kashpitan@googlemail.com>
2020-04-28 02:24:33 +0100

    delete lyric function, clear all lyrics function, minor styling and colour changes

--- client/src/Context/Reducer.js
+++ client/src/Context/Reducer.js
@@ -1,4 +1,9 @@
-import { SAVE_LYRICS, NONE_SELECTED_ALERT } from "../Context/Types";
+import {
+  SAVE_LYRICS,
+  NONE_SELECTED_ALERT,
+  CLEAR_LYRICS,
+  DELETE_LYRIC_ITEM,
+} from "../Context/Types";
 
 export default (state, action) => {
   switch (action.type) {
@@ -8,9 +13,24 @@ export default (state, action) => {
         savedLyrics: [...state.savedLyrics, action.payload],
       };
     case NONE_SELECTED_ALERT:
+      // console.log("none selected");
+
       return {
         ...state,
         noneSelectedAlert: action.payload,
+      };
+    case CLEAR_LYRICS:
+      return {
+        ...state,
+        savedLyrics: [],
+      };
+    case DELETE_LYRIC_ITEM:
+      const newSavedLyrics = state.savedLyrics.filter((lyricObj) => {
+        return lyricObj.id !== action.payload;
+      });
+      return {
+        ...state,
+        savedLyrics: newSavedLyrics,
       };
 
     default:
