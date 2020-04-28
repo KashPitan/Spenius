044dac6a48bc3334150bc7be9fdc731a636ed875
Kash Pitan <kashpitan@googlemail.com>
2020-04-28 22:51:59 +0100

    server changes

--- app.js
+++ app.js
@@ -54,6 +54,16 @@ app
 
 app.use("/lyrics", LyricRoutes);
 
+//serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.get("/login", function (req, res) {
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
@@ -166,16 +176,6 @@ app.get("/refresh_token", function (req, res) {
     }
   });
 });
-
-//serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 // console.log("Listening on 8888");
 app.listen(PORT);
